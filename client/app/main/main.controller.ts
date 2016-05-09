35f185fcb3c1cd92304fd3a6498c17a34d3ef7b9
Daniel Carmichael <daniel.carmichael1@gmail.com>
2016-05-09 16:48:09 -0400

    more modifs

--- client/app/main/main.controller.ts
+++ client/app/main/main.controller.ts
@@ -7,17 +7,29 @@
     constructor($http) {
       this.$http = $http;
       this.eventLocations = [];
+      this.dataPoints = [];
       var styleArray = [{ "featureType": "all", "elementType": "labels.text.fill", "stylers": [{ "color": "#ffffff" }, { "weight": "0.20" }, { "lightness": "28" }, { "saturation": "23" }, { "visibility": "off" }] }, { "featureType": "all", "elementType": "labels.text.stroke", "stylers": [{ "color": "#494949" }, { "lightness": 13 }, { "visibility": "off" }] }, { "featureType": "all", "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }, { "featureType": "administrative", "elementType": "geometry.fill", "stylers": [{ "color": "#000000" }] }, { "featureType": "administrative", "elementType": "geometry.stroke", "stylers": [{ "color": "#144b53" }, { "lightness": 14 }, { "weight": 1.4 }] }, { "featureType": "landscape", "elementType": "all", "stylers": [{ "color": "#08304b" }] }, { "featureType": "poi", "elementType": "geometry", "stylers": [{ "color": "#0c4152" }, { "lightness": 5 }] }, { "featureType": "road.highway", "elementType": "geometry.fill", "stylers": [{ "color": "#000000" }] }, { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [{ "color": "#0b434f" }, { "lightness": 25 }] }, { "featureType": "road.arterial", "elementType": "geometry.fill", "stylers": [{ "color": "#000000" }] }, { "featureType": "road.arterial", "elementType": "geometry.stroke", "stylers": [{ "color": "#0b3d51" }, { "lightness": 16 }] }, { "featureType": "road.local", "elementType": "geometry", "stylers": [{ "color": "#000000" }] }, { "featureType": "transit", "elementType": "all", "stylers": [{ "color": "#146474" }] }, { "featureType": "water", "elementType": "all", "stylers": [{ "color": "#021019" }] }]
-      this.map = {  center: { latitude: 30, longitude: 0 },
+      this.map =  { center: { latitude: 30, longitude: 0 },
                     zoom: 3,
                     options: { styles: styleArray },
-                    showHeat: true};
+                    showHeat: true
+                  };
     }
 
     $onInit() {
       this.$http.get('/api/coordinates').then(response => {
         this.eventLocations = response.data;
       });
+
+      eventLocations.forEach(function(event){
+        var newPoint = {
+          location: new google.maps.LatLng(event.latitude, event.longitude),
+          weight: 100000
+        }
+        dataPoints.push(newPoint);
+      });
+
+
       this.map.heatLayerCallback = function (layer) {
           var mockHeatLayer = new MockHeatLayer(layer);
           }
