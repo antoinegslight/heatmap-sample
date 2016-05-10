6275fd68f822334c6b7ab52044b7d5a250613d07
antoine.gslight <antoine.gslight@ticketmaster.com>
2016-05-10 10:52:55 -0400

    remove default UI

--- client/app/main/main.controller.ts
+++ client/app/main/main.controller.ts
@@ -11,7 +11,10 @@
       var styleArray = [{ "featureType": "all", "elementType": "labels.text.fill", "stylers": [{ "color": "#ffffff" }, { "weight": "0.20" }, { "lightness": "28" }, { "saturation": "23" }, { "visibility": "off" }] }, { "featureType": "all", "elementType": "labels.text.stroke", "stylers": [{ "color": "#494949" }, { "lightness": 13 }, { "visibility": "off" }] }, { "featureType": "all", "elementType": "labels.icon", "stylers": [{ "visibility": "off" }] }, { "featureType": "administrative", "elementType": "geometry.fill", "stylers": [{ "color": "#000000" }] }, { "featureType": "administrative", "elementType": "geometry.stroke", "stylers": [{ "color": "#144b53" }, { "lightness": 14 }, { "weight": 1.4 }] }, { "featureType": "landscape", "elementType": "all", "stylers": [{ "color": "#08304b" }] }, { "featureType": "poi", "elementType": "geometry", "stylers": [{ "color": "#0c4152" }, { "lightness": 5 }] }, { "featureType": "road.highway", "elementType": "geometry.fill", "stylers": [{ "color": "#000000" }] }, { "featureType": "road.highway", "elementType": "geometry.stroke", "stylers": [{ "color": "#0b434f" }, { "lightness": 25 }] }, { "featureType": "road.arterial", "elementType": "geometry.fill", "stylers": [{ "color": "#000000" }] }, { "featureType": "road.arterial", "elementType": "geometry.stroke", "stylers": [{ "color": "#0b3d51" }, { "lightness": 16 }] }, { "featureType": "road.local", "elementType": "geometry", "stylers": [{ "color": "#000000" }] }, { "featureType": "transit", "elementType": "all", "stylers": [{ "color": "#146474" }] }, { "featureType": "water", "elementType": "all", "stylers": [{ "color": "#021019" }] }]
       this.map =  { center: { latitude: 30, longitude: 0 },
                     zoom: 3,
-                    options: { styles: styleArray },
+                    options: {
+                      styles: styleArray,
+                      disableDefaultUI: true
+                    },
                     showHeat: true,
                     heatLayerCallback: function (layer) {
                        $http.get('/api/coordinates').then(response => {
@@ -29,7 +32,7 @@
                           dissipating: true,
                           radius: 10,
                           opacity: 0.8,
-                          maxIntensity: 50
+                          maxIntensity: 50,
                           gradient: [
                           'rgba(0, 255, 255, 0)',
                           'rgba(0, 255, 255, 1)',
@@ -45,7 +48,7 @@
                           'rgba(127, 0, 63, 1)',
                           'rgba(191, 0, 31, 1)',
                           'rgba(255, 0, 0, 1)'
-                        ]
+                          ]
                         });
                     })
                   }
